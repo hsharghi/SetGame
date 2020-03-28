0e45f7dcf8047c5e8fd18712e4667a919f34b766
Hadi Sharghi <hsharghi@gmail.com>
2020-03-28 13:09:59 +0430

    add fade texture extension to SKSpriteNode

--- SetGame/SKSpriteNode+Extensions.swift
+++ SetGame/SKSpriteNode+Extensions.swift
@@ -11,8 +11,8 @@ import SpriteKit
 extension SKSpriteNode {
     func drawBorder(color: UIColor, width: CGFloat, offset: CGFloat = 0, name: String? = nil) {
         let borderFrame = offset > 0 ?
-        frame.offsetBy(dx: offset, dy: offset) :
-        frame.insetBy(dx: offset, dy: offset)
+            frame.offsetBy(dx: offset, dy: offset) :
+            frame.insetBy(dx: offset, dy: offset)
         let shapeNode = SKShapeNode(rect: borderFrame)
         shapeNode.fillColor = .clear
         shapeNode.strokeColor = color
@@ -20,5 +20,27 @@ extension SKSpriteNode {
         shapeNode.name = name
         addChild(shapeNode)
     }
+    
+    func fadeTexture(to newTexture: SKTexture, duration: CFTimeInterval) {
+        let fadeInSpriteNode = fadeInSprite(with: newTexture, spriteNode: self)
+        self.parent?.addChild(fadeInSpriteNode)
+        fadeInSpriteNode.run(SKAction.sequence([
+            SKAction.fadeAlpha(to: 1, duration: duration),
+            SKAction.run {
+                fadeInSpriteNode.removeFromParent()
+                self.texture = newTexture
+            }
+        ]))
+    }
+    
+    private func fadeInSprite(with texture: SKTexture, spriteNode: SKSpriteNode) -> SKSpriteNode {
+        let fadeInSprite = SKSpriteNode(texture: texture, size: spriteNode.size)
+        fadeInSprite.alpha = 0
+        fadeInSprite.anchorPoint = spriteNode.anchorPoint
+        fadeInSprite.position = spriteNode.position
+        
+        return fadeInSprite
+    }
+    
 }
 
